44f06f28367181bd5bb612277b6345cc26978bda
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-07-14 02:54:30 +0600

    second commit

--- vector next permutation in c++.cpp
+++ vector next permutation in c++.cpp
@@ -1,25 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
+void print(vector<int> &v, int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
 int main()
 {
-    vector<int>v;
-    int n;
-    cin>>n;
-    int x;
-    for(int i=0; i<n; i++)
+    vector<int> v;
+    int n, a;
+    cin >> n;
+    for (int i = 0; i < n; i++)
     {
-        cin>>x;
-        v.push_back(x);//value vector a dilam
+        cin >> a;
+        v.push_back(a);
     }
-
-    sort(v.begin(), v.end());//vector ar value ta sort kore nilam
-
-    while(next_permutation(v.begin(), v.end()))//permutation korlam
+    sort(v.begin(), v.end());
+    while (next_permutation(v.begin(), v.end()))
     {
-        for(int i=0; i<n; i++)
-        {
-            cout<<v[i]<<" ";//vector print korlam
-        }
-        cout<<endl;
+        print(v, n);
     }
 }
