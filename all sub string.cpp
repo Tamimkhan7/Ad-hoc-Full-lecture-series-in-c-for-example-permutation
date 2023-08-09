cfebbd1d5be3a0cd0536c3cc01141b6c10d2a556
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-08-10 04:17:40 +0600

    second commit

--- all sub string.cpp
+++ all sub string.cpp
@@ -1,28 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define faster                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define sq(x) (x) * (x)
+#define PI acos(-1.0)
+#define all(x) x.begin(), x.end()
+#define mod 1000000007
+typedef long long int ll;
+typedef unsigned long long int llu;
+#define pb push_back
+#define pop pop_back()
+#define len(a) sizeof(a)
+#define lc (n * 2)
+#define rc ((n * 2) + 1)
 int main()
 {
+    faster;
     int t;
-    cin>>t;
-    while(t--)//test case
+    cin >> t;
+    while (t--)
     {
-        string s;//string
-        cin>>s;
-        int len = s.size();//string size
-        int c=0;
-        for(int i=0; i<len; i++)//first index kore ber korteci
+        string s;
+        cin >> s;
+        int c = 0;
+        int len = s.size();
+        for (int i = 0; i < len; i++)
         {
-            for(int j=i; j<len; j++)//i ar value thek len index projnto value cout korteci
+            for (int j = i; j < len; j++)
             {
-                for(int k=i; k<=j; k++)//i and j ar value compare kore k ar value print korteci
+                for (int k = i; k <= j; k++)
                 {
-                    cout<<s[k];
+                    cout << s[k];
                 }
-                c++;//counter bosaici jate koyta value print hoyece count korte pari
-                cout<<endl;
+                c++;
+                cout << endl;
             }
         }
-        cout<<c<<endl;//counter value print korteci
+        cout << c << endl;
     }
-
 }
+/*int t;
+cin >> t;
+while (t--) // test case
+{
+    string s; // string
+    cin >> s;
+    int len = s.size(); // string size
+    int c = 0;
+    for (int i = 0; i < len; i++) // first index kore ber korteci
+    {
+        for (int j = i; j < len; j++) // i ar value thek len index projnto value cout korteci
+        {
+            for (int k = i; k <= j; k++) // i and j ar value compare kore k ar value print korteci
+            {
+                cout << s[k];
+            }
+            c++; // counter bosaici jate koyta value print hoyece count korte pari
+            cout << endl;
+        }
+    }
+    cout << c << endl; // counter value print korteci
+}*/
--- all subset in c++.cpp
+++ all subset in c++.cpp
@@ -1,24 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define faster                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define sq(x) (x) * (x)
+#define PI acos(-1.0)
+#define all(x) x.begin(), x.end()
+#define mod 1000000007
+typedef long long int ll;
+typedef unsigned long long int llu;
+#define pb push_back
+#define pop pop_back()
+#define len(a) sizeof(a)
+#define lc (n * 2)
+#define rc ((n * 2) + 1)
 int main()
 {
+    faster;
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    int ct = pow(2,n);
-    for(int i=0; i<ct; i++)
+    int ct = pow(2, n);
+    for (int i = 0; i < ct; i++)
     {
-        for(int j=0; j<n; j++)
+        for (int j = 0; j < n; j++)
         {
-            if((i & (i << j)) != 0)
+            if ((i & (i << j)) != 0) // this line means , suppose i=1, i<<j menas, suppose j=1, like that= 1*2, j time 2 value dara gun i ar valueke, tahole ans cole asbe
             {
-                cout<<arr[j]<<" ";
+                cout << arr[j] << " ";
             }
         }
-        cout<<endl;
+        cout << endl;
     }
+    return 0;
 }
